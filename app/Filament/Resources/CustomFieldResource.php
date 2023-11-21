885acbedbbbe9559db460819382c9eb77693df27
divya.patange@ranium.in <divya.patange@ranium.in>
2023-11-21 12:52:50 +0530

    CRUD for custom field

--- app/Filament/Resources/CustomFieldResource.php
+++ app/Filament/Resources/CustomFieldResource.php
@@ -6,9 +6,11 @@ use App\Filament\Resources\CustomFieldResource\Pages;
 use App\Filament\Resources\CustomFieldResource\RelationManagers;
 use App\Models\CustomField;
 use Filament\Forms;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -17,13 +19,18 @@ class CustomFieldResource extends Resource
 {
     protected static ?string $model = CustomField::class;
 
+    protected static ?string $navigationGroup = 'Settings';
+
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                TextInput::make('name')
+                    ->required()
+                    ->autofocus()
+                    ->unique(ignoreRecord: true)
             ]);
     }
 
@@ -31,13 +38,22 @@ class CustomFieldResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->toggleable()
+                    ->sortable(),
+                TextColumn::make('updated_at')
+                    ->toggleable()
+                    ->sortable()
             ])
             ->filters([
                 //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make()
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
--- app/Filament/Resources/CustomFieldResource/Pages/CreateCustomField.php
+++ app/Filament/Resources/CustomFieldResource/Pages/CreateCustomField.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateCustomField extends CreateRecord
 {
     protected static string $resource = CustomFieldResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
